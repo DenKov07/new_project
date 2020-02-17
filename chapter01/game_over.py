cdeb9fb5163fb74b4528bb90094fe5bb4a4e646e
den <DenKa0794@gmail.com>
2020-02-17 14:18:52 +0300

    Уже обсуждали

---
+++ chapter01/game_over.py
@@ -0,0 +1,7 @@
+# Game Over
+# Demonstrates the print function
+# Программу писал Денис Качановский 5 февраля 2020 года
+
+print('Сегодня это, завтра то')
+
+input('\nНажмите Enter чтобы выйти')
---
+++ chapter02/greeter.py
@@ -0,0 +1,13 @@
+# Greeter
+# Demonstrates the use of a variable
+
+name = input('Эй, как тебя зовут, красавец?\n')
+
+print('Крутое имя,', name)
+
+input('\n\nТычь Enter, если захочешь выйти, Бро')
+# print(name)
+# name = "Larry"
+# print("Hi,", name)
+#
+# input("\n\nPress the enter key to exit.")
