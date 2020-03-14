1dd08b4684a4982ab0ddba0ec0d09ba04666cbd3
DenKov07 <DenKa0794@gmail.com>
2020-03-14 12:54:12 +0300

    Task_4.2
    Yeap, print(text[::1]) working... so... -_-

--- chapter04/Task_4_2.py
+++ chapter04/Task_4_2.py
@@ -1,8 +1,9 @@
 # The program outputs the text in reverse
 
 text = input('Enter text\n')
+print(text[::-1])
 
-while text:
-    print(text[-1], end='')
-    text = text[:-1]
+# while text:
+#     print(text[-1], end='')
+#     text = text[:-1]
 
